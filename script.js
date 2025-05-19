a8d31b838d249b97e8a2bc4aad9fcccfabb6a1e1
NagrajX <satishnagraj54@gamil.com>
2025-05-19 21:05:15 +0530

    adding some more feature"

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
- 
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelector(".game-Info");
+const newGameBtn = document.querySelector(".btn");
+
+
+let currentPlayer;
+let gameGrid;
+
+
+const winningPositions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+
+];
+
+// let create the function to initilize the game
+
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["", "", "", "", "", "", "", "", ""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+}
+
+initGame();
+
